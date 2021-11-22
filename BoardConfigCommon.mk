facff7431354f655ddbcc69db1ef1ed1885862e4
remusWulf <remuswulf@hotmail.com>
2021-11-22 03:22:18 +0000

    Fix building of ELF prebuilts.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,6 +11,8 @@ COMMON_PATH := device/xiaomi/sdm845-common
 # Build broken
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 
 # Architecture
 TARGET_ARCH := arm64
